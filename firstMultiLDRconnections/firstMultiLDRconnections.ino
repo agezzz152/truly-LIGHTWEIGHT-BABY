b48460147a8bb188095f5661c64d4420b3d9bf6e
Galch <galevenzur2@gmail.com>
2023-11-05 00:56:08 +0200

    Test for multiple LDR's and neopixel

--- firstMultiLDRconnections/firstMultiLDRconnections.ino
+++ firstMultiLDRconnections/firstMultiLDRconnections.ino
@@ -1,35 +1,63 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 5
-#define LEDNUM 32
-#define delayval 50
-#define photores 34
-Adafruit_NeoPixel pixels(LEDNUM, PIN, NEO_GRB + NEO_KHZ800);
+#define PIN 5          // Define the pin you're using to control the Neopixels
+#define NUM_PIXELS 48  // Define the number of Neopixels in your strip
+
+#define NUM_LDR 6
+
+int const LDR[] = { 15, 2, 4, 13, 12, 14 };  //from the rightest LDR clockwise
+float LDRval[NUM_LDR];
+
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
 
 void setup() {
-  pixels.setBrightness(100);
-  pixels.begin();
   Serial.begin(115200);
-  pixels.setBrightness(150);
-  delay(200);
+
+  strip.begin();
+  strip.show();  // Initialize all pixels to 'off'
+  strip.setBrightness(100);
 }
+
+int signOFx(int x) {
+  return x / abs(x);
+}
+
+
+
+int LDRcheck(int conn) {
+  int a = analogRead(conn);
+  return a;
+}
+
+int whichLDR(float vals[], int n) {
+  for (int i = 0; i < n; i++) {
+    if (vals[i] > 500)
+      return i;
+  }
+
+  return -1;
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t color, int wait) {
+  for (int i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, color);
+    strip.show();
+  }
+}
+
 
 void loop() {
-  LightUp();
-  int reading = analogRead(photores);
-  Serial.println(reading);
-  delay(delayval);
-}
-
-
-void LightUp() {
-  for (int j = 0; j < LEDNUM; j++){
-    pixels.setPixelColor(j, pixels.Color(0,0,30));
-    }
-    pixels.show();
-}
-
-void LightOff() { 
-  pixels.clear();
-  pixels.show();
+  colorWipe(strip.Color(30, 0, 0), 50);
+  for (int i = 0; i < NUM_LDR; i++) {
+    LDRval[i] = LDRcheck(LDR[i]);
+    Serial.print(LDRval[i]);
+    Serial.print(" - ");
+  }
+  int z = whichLDR(LDRval, NUM_LDR);
+  Serial.print("| ");
+  Serial.print(z);
+  Serial.println(" |");
 }
