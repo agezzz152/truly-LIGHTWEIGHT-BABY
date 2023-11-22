f396c6297b58e04ebccfbc4638d87fd6d665bfe4
Galch <galevenzur2@gmail.com>
2023-11-22 23:30:34 +0200

    button and led working codegit statusgit status

--- connection of 2 arduino's/RecieverCode/RecieverCode.ino
+++ connection of 2 arduino's/RecieverCode/RecieverCode.ino
@@ -1,12 +1,15 @@
-char mystr[10]; //Initialized variable to store recieved data
+int mystr; //Initialized variable to store recieved data
 
 void setup() {
   // Begin the Serial at 9600 Baud
+  Serial1.begin(9600);
   Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  Serial.readBytes(mystr,5); //Read the serial data and store in var
+  mystr = Serial1.read(); //Read the serial data and store in var
   Serial.println(mystr); //Print data on Serial Monitor
-  delay(1000);
+  delay(20);
+  digitalWrite(LED_BUILTIN, mystr);
 }
--- connection of 2 arduino's/TransmitionCode/TransmitionCode.ino
+++ connection of 2 arduino's/TransmitionCode/TransmitionCode.ino
@@ -1,11 +1,25 @@
-char mystr[5] = "Hello"; //String data
-
+int mystr = 1;  //String data
+int swtch = 11;
 void setup() {
   // Begin the Serial at 9600 Baud
-  Serial.begin(9600);
+  pinMode(swtch, INPUT);
+  Serial1.begin(9600);
 }
 
 void loop() {
-  Serial.write(mystr,5); //Write the serial data
-  delay(1000);
+  // mystr = 0;
+  // for (int i = 0; i < 20; i++) {
+  //   delay(10);
+  // }
+  mystr = checkSwitch(swtch);
+  Serial1.write(mystr);  //Write the serial data
+  // for (int i = 0; i < 20; i++) {
+  //   Serial1.write(mystr);  //Write the serial data
+  //   delay(10);
+  // }
+}
+
+bool checkSwitch(int a) {
+  bool bul = (bool) digitalRead(a);
+  return bul;
 }
