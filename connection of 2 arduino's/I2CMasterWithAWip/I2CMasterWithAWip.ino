b6e845f7d9ec766232d7de1c22c7c43bc90225c4
Galch <galevenzur2@gmail.com>
2024-01-21 16:30:59 +0200

    Added correct i2c codes yes masetfr

--- connection of 2 arduino's/I2CMasterWithAWip/I2CMasterWithAWip.ino
+++ connection of 2 arduino's/I2CMasterWithAWip/I2CMasterWithAWip.ino
@@ -2,14 +2,14 @@
 
 void setup() {
   Wire.begin(8);                 // join i2c bus with address #8
-  Wire.onRequest(requestEvent);  // register event
+  Wire.onRequest(requestEvent); // register event
 }
 
 void loop() {
   int val;
-  val = analogRead(A0);  //connect grayscale sensor to Analog 0
   requestEvent(val);
 }
 void requestEvent(int val) {
+  val = analogRead(A0);
   Wire.write(val);  //print the value to serial
 }
--- connection of 2 arduino's/I2CSlaveySlave/I2CSlaveySlave.ino
+++ connection of 2 arduino's/I2CSlaveySlave/I2CSlaveySlave.ino
@@ -2,14 +2,21 @@
 
 void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
+  pinMode(LED_BUILTIN, OUTPUT);
+
   Serial.begin(9600);  // start serial for output
 }
 
 void loop() {
-  Wire.requestFrom(8, 6);    // request 6 bytes from peripheral device #8
-
+  Wire.requestFrom(8, 1);    // request 6 bytes from peripheral device #8
   while (Wire.available()) { // peripheral may send less than requested
     int c = Wire.read(); // receive a byte as character
     Serial.println(c);         // print the character
+   if (c>=100){
+  digitalWrite(LED_BUILTIN, HIGH);
   }
+  else {
+  digitalWrite(LED_BUILTIN, LOW);
+  }
+ }
 }
