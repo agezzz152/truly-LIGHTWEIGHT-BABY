511f948a7f3f1256030581b391fbe73e2ec16774
Galch <galevenzur2@gmail.com>
2024-03-31 17:19:57 +0300

    Complete overhaul for IR, uses IRlib library. issue: detecting causes detection loop

--- IReallylikeboys/IReallylikeboys.ino
+++ IReallylikeboys/IReallylikeboys.ino
@@ -1,62 +1,46 @@
-  unsigned long previousMillis = 0;  // variable to store the previous time
-  unsigned long eventStartTime = 0;  // variable to store the start time of the special event
-  const int eventDuration = 10;      // duration of the special event (in milliseconds)
-  const int interval = 1000;         // interval at which to do something (in milliseconds)
-  int IRPin = 2;
-  int VCCpin = 6;
-  int lastSeenChange = 0;
-  int lastVal = 1;
+/* dump.ino Example sketch for IRLib2
+   Illustrates how to receive an IR signal, decode it and print
+   information about it to the serial monitor.
+*/
+//This includes everything. Not generally recommended.
+//It's better to include only the parts of library you really need.
+//But for this example it's quick and easy. See "comboDump" example
+//for a more efficient way.
+#include "IRLibAll.h"
+int IRPin = 2;
 
+int timeSinceChange = 0;
+IRrecvPCI myReceiver(IRPin);  //create receiver and pass pin number
+IRdecode myDecoder;           //create decoder
+int IR1 = 0;
 
-  void setup() {
-    // initialize serial communication:
-    Serial.begin(9600);
-    pinMode(IRPin, INPUT);
-    pinMode(VCCpin, OUTPUT);
-  }
+void setup() {
+  Serial.begin(9600);
+  delay(2000);
+  while (!Serial)
+    ;                       //delay for Leonardo
+  myReceiver.enableIRIn();  // Start the receiver
+  Serial.println(F("Ready to receive IR signals"));
+}
 
-  void loop() {
-    // get the current time
-    unsigned long currentMillis = millis();
+void loop() {
+  //Continue looping until you get a complete signal received
+  int val = checkIR(IRPin);
+  if (val == 1)
+    timeSinceChange = 0;
 
+  if (timeSinceChange < 25)
+    IR1 = 1;
 
+  timeSinceChange++;
+  displayIR();
+  myReceiver.enableIRIn();  //Restart receiver
+}
 
-    int CurrentVal = digitalRead(IRPin);
-    if (lastVal ^ CurrentVal)
-      lastSeenChange = 0;
-    else
-      lastSeenChange++;
-    // print out the state of the button:
-    delay(1);  // delay in between reads for stability
+void displayIR(){
+  Serial.println(IR1);
+}
 
-    if (lastSeenChange < 5)
-      Serial.print("1 ");
-    else
-      Serial.print("0 ");
-
-    Serial.println(CurrentVal);
-
-    lastVal = CurrentVal;
-    digitalWrite(VCCpin, 1);
-
-    // check if 1000 milliseconds have passed
-    if (currentMillis - previousMillis >= interval) {
-      previousMillis = currentMillis;
-      digitalWrite(VCCpin, 0);
-
-      myDelay(10);
-    }
-  }
-
-
-
-  void myDelay(int millDelay) {
-    int startTime = millis();
-    int currentT;
-    while (true) {
-      currentT = millis();
-      if (currentT >= startTime + millDelay) {
-        break;
-      }
-    }
-  }
+bool checkIR(int PIN) {
+  return myReceiver.getResults();
+}
