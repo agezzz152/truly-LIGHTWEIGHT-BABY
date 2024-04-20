927e25643cf22024a64750df287534c56cbd1662
Galch <galevenzur2@gmail.com>
2024-04-21 00:16:35 +0300

    fixed bug with moving average and added 4th sensor

--- IReallylikeboys/IReallylikeboys.ino
+++ IReallylikeboys/IReallylikeboys.ino
@@ -1,9 +1,10 @@
 
 #include "MovingAverage.h"
 
+#define NUM_IR 4
 
+MovingAverage IR[NUM_IR];
 
-MovingAverage a(20);
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -15,15 +16,16 @@ void loop() {
   // read the input on analog pin 0:
   float sensorValue1 = analogRead(A1);
   // print out the value you read:
-  int Right = a.updateData(sensorValue1);
+  int Right = IR[0].updateData(sensorValue1);
 
   float sensorValue2 = analogRead(A2);
-  int Middle = a.updateData(sensorValue2);
+  int Middle = IR[1].updateData(sensorValue2);
 
   float sensorValue3 = analogRead(A3);
-  int Left = a.updateData(sensorValue3);
+  int Left = IR[2].updateData(sensorValue3);
 
+  float sensorValue4 = analogRead(A4);
+  int extra = IR[3].updateData(sensorValue4);
 
-
-  Serial.print(Left); Serial.print(" | "); Serial.print(Middle); Serial.print(" | "); Serial.println(Right);
+  Serial.print(Left); Serial.print(" | "); Serial.print(Middle); Serial.print(" | "); Serial.print(Right); Serial.print(" | "); Serial.println(extra);
 }
--- IReallylikeboys/MovingAverage.h
+++ IReallylikeboys/MovingAverage.h
@@ -7,7 +7,7 @@ private:
     int sizeOfDataArray;
     int count;
 public:
-    MovingAverage(int num_array);
+    MovingAverage(int num_array = 20);
     float updateData(float value);
     void reset(void);
 };
